c702a509ec33a7f293feec375b082f48a8a06fbb
Viktor <DarmokhvalViktor@gmail.com>
2023-08-15 22:22:34 +0300

    Init commit

---
+++ Lesson_1_homework/main.js
@@ -0,0 +1,59 @@
+let s1 = "hello";
+let s2 = "owu";
+let s3 = "com";
+let s4 = "ua";
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+
+let b1 = true;
+let b2 = false;
+
+console.log(b1);
+console.log(b2);
+
+
+
+let firstName = "Viktor";
+let middleName = "Darmokhval";
+let lastName = "Zinoviyovich";
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+console.log(typeof person);
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+
+let name = prompt("Enter your name: ");
+let last_name = prompt("Enter your lastname: ");
+let age = prompt("Enter your age: ");
+
+console.log(name);
+console.log(last_name);
+console.log(age);
